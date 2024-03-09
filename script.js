3003895b4daad47a8c72be0d42ca773fca7f5fc7
Atif452 <147477867+Atif452@users.noreply.github.com>
2024-03-09 14:46:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let btn=document.querySelector(".lines")
+btn.addEventListener("click",function(){
+    document.querySelector(".lines").style.display="none"
+    document.querySelector(".nav").className="navBar"
+})
